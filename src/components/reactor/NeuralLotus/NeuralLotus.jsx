50ce2f403cdf67487df3754d2a3a1cb1804ae6e5
Adamya Pandey <adamya.16.pandey@gmail.com>
2026-07-23 02:23:30 +0530

    design system

--- src/components/reactor/NeuralLotus/NeuralLotus.jsx
+++ src/components/reactor/NeuralLotus/NeuralLotus.jsx
@@ -9,13 +9,27 @@ export default function NeuralLotus() {
             width="160"
             height="160"
         >
-            {Array.from({ length: 8 }).map((_, i) => (
-                <LotusPetal
-                    key={i}
-                    rotation={i * 45}
-                    distance={52 }
-                />
-            ))}
+            <>
+  {/* Secondary Petals */}
+  {Array.from({ length: 6 }).map((_, i) => (
+    <LotusPetal
+      key={`secondary-${i}`}
+      rotation={i * 60 + 30}
+      distance={44}
+      scale={0.72}
+    />
+  ))}
+
+  {/* Primary Petals */}
+  {Array.from({ length: 6 }).map((_, i) => (
+    <LotusPetal
+      key={`primary-${i}`}
+      rotation={i * 60}
+      distance={52}
+      scale={1}
+    />
+  ))}
+</>
         </svg>
     );
 }

a7292e4079a52fae2b00acdbb5862a0b873fa526
Adamya Pandey <adamya.16.pandey@gmail.com>
2026-08-02 18:19:19 +0530

    aether: phase 1

---
+++ src/aether/layout/PresenceField.jsx
@@ -0,0 +1,9 @@
+import "./PresenceField.css";
+
+export default function PresenceField({ children }) {
+    return (
+        <section className="aether-presence-field">
+            {children}
+        </section>
+    );
+}
---
+++ src/aether/layout/SpatialCanvas.jsx
@@ -0,0 +1,9 @@
+import "./SpatialCanvas.css";
+
+export default function SpatialCanvas({ children }) {
+    return (
+        <main className="aether-spatial-canvas">
+            {children}
+        </main>
+    );
+}
